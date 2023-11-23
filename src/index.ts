feb9539b2062292cfa4629c0d4f63fe6ad129681
dseydel <daniel.seydel@ionos.com>
2023-11-23 13:29:34 +0100

    Refactor URL routing in src/index.ts
    URL routing in src/index.ts has been updated to improve error handling and readability. This includes segregating the validation checks into more granular checks; differentiating 'server' routes first, and then 'room' routes, allowing for more specific error messages and status codes. This also incorporates a 404 status response for invalid server routes and improves logging for missing username headers. These changes should enhance user feedback and ease future development.

--- src/index.ts
+++ src/index.ts
@@ -9,29 +9,36 @@ const server = Bun.serve<ClientData>({
       const url = new URL(request.url)
       let routes = url.pathname.split("/")
       // Rooms will be available via /room/<room-uuid>
-      if ( routes[1].toLowerCase() === 'server' && routes[2].toLowerCase() === 'room') {
-         if(routes.length < 4) {
-            return new Response(JSON.stringify({message: "No roomId provided"}), {status: 400})
+      if (routes[1].toLowerCase() === 'server') {
+
+         if (routes.length < 3) {
+            return new Response(JSON.stringify({message: 'Page not found'}), {status: 404});
          }
 
-         let roomId = routes[3].toLowerCase();
-         if(!roomId.match(/[a-z\d-]{6,}/)) {
-            return new Response(JSON.stringify({message: "Invalid roomId provided"}), {status: 400})
-         }
-
-         // Check if a username is provided
-         if(!request.headers.has('X-Username')) {
-            logger.warning("Request with missing header was sent")
-            return new Response(JSON.stringify({message: "Username is required"}), {status: 400})
-         }
-
-         const success = server.upgrade(request, {
-            data: {
-               roomId: roomId,
-               username: request.headers.get('X-Username')
+         if (routes[2].toLowerCase() === 'room') {
+            if (routes.length < 4) {
+               return new Response(JSON.stringify({message: "No roomId provided"}), {status: 400})
             }
-         });
-         return success ? undefined : new Response(JSON.stringify({message: "WebSocket upgrade error"}), { status: 400 });
+
+            let roomId = routes[3].toLowerCase();
+            if (!roomId.match(/[a-z\d-]{6,}/)) {
+               return new Response(JSON.stringify({message: "Invalid roomId provided"}), {status: 400})
+            }
+
+            // Check if a username is provided
+            if (!request.headers.has('X-Username')) {
+               logger.warning("Request with missing header was sent")
+               return new Response(JSON.stringify({message: "Username is required"}), {status: 400})
+            }
+
+            const success = server.upgrade(request, {
+               data: {
+                  roomId: roomId,
+                  username: request.headers.get('X-Username')
+               }
+            });
+            return success ? undefined : new Response(JSON.stringify({message: "WebSocket upgrade error"}), {status: 400});
+         }
       } else if(routes[1].toLowerCase() === 'health') {
          return handleHealthEndpoints(routes)
       }
